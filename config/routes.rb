062c6f1cd9bd7ceadf18d1742fe16536b88fc73f
mkalenik <kalina714@gmail.com>
2023-03-09 20:13:25 +0100

    Added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'static/terms'
+  get 'static/privacy'
+  get 'static/shipping'
+  get 'static/about'
+  get 'categories/show'
+  get 'products/index'
+  get 'products/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
